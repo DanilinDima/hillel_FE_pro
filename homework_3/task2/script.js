dfd99f06a1e2c8608694fc7708316354693eb3db
DanilinDima <160770583+DanilinDima@users.noreply.github.com>
2025-03-09 13:57:41 +0200

    task2 update

--- homework_3/task2/script.js
+++ homework_3/task2/script.js
@@ -1,7 +1,8 @@
-let str1 = "First string";
-let str2 = "Second string";
-let str3 = "Third string";
+let str1 = prompt("Enter first string:");
+let str2 = prompt("Enter second string:");
+let str3 = prompt("Enter third string:");
 
 let randomString = [str1, str2, str3].sort(() => Math.random() - 0.5);
 
 console.log(`Strings in random order: ${randomString.join(", ")}`); 
+alert(`Strings in random order: ${randomString.join(", ")}`);
